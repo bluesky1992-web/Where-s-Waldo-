46ad5c9661770bce5bac059906b92afbf9a78ced
bluesky1992-web <113296754+bluesky1992-web@users.noreply.github.com>
2022-11-01 22:22:30 +0000

    some changes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const example = [
   ["A", "A", "A"],
   ["B", "A", "A"]
 ];
+console.log(example[2][1])
 
 const whereIsWaldo = (matrix) => {
   for (let i = 0; i < matrix.length; i++) {
     console.log(matrix[i])
-    for (let j = 0; j < matrix[i][j].length; j++) {
+    for (let j = 0; j < matrix[i]; j++) {
       console.log(matrix[i][j])
 
     }
